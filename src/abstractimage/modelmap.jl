2570091ed1dc4cde5381bfbfa688e61f31a61e70
Kazunori Akiyama <kakiyama@mit.edu>
2023-02-16 13:21:37 -0500

    fix minor typos

--- src/abstractimage/modelmap.jl
+++ src/abstractimage/modelmap.jl
@@ -12,7 +12,7 @@ function map!(
     xg, yg = get_xygrid(image)
 
     # grid size
-    dxdy = im.metadata[:dx] * im.metadata[:dy]
+    dxdy = image.metadata[:dx] * image.metadata[:dy]
 
     # mapout kernel
     imarray = Matrix{Float64}(undef, length(xg), length(yg))
@@ -33,11 +33,11 @@ function add!(
     xg, yg = get_xygrid(image)
 
     # grid size
-    dxdy = im.metadata[:dx] * im.metadata[:dy]
+    dxdy = image.metadata[:dx] * image.metadata[:dy]
 
     # mapout kernel
     imarray = Matrix{Float64}(undef, length(xg), length(yg))
     @floop ex for xidx = 1:nx, yidx = 1:ny
-        @inbounds image.data[xidx, yidx, :, :, :] = intensity_point(model, xg[xidx], yg[yidx]) * dxdy
+        @inbounds image.data[xidx, yidx, :, :, :] += intensity_point(model, xg[xidx], yg[yidx]) * dxdy
     end
 end
